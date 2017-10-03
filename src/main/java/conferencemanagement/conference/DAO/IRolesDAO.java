d16668464b8f0bd0dd343f3691c8990544c7105b
Petar Mitrevski <mitrevski.p@gmail.com>
2017-10-03 11:22:10 +0200

    Initial commit

---
+++ src/main/java/conferencemanagement/conference/DAO/IRolesDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conferencemanagement.conference.DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author Petar
+ */
+public interface IRolesDAO {
+    
+    public List<Roles> getAllRoles();
+    public Roles getRolesById(int rolesId);
+    public void updateRoles(Roles roles);
+    public void deleteRoles(Roles roles);
+    boolean rolesExists();
+}
