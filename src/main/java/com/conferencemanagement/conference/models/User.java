c0910b35d5f2f59d5f61526bde302032f1764ff2
Mario <32096232+MarioBozinovski93@users.noreply.github.com>
2017-10-05 12:43:50 +0200

    One-to-one

--- src/main/java/com/conferencemanagement/conference/models/User.java
+++ src/main/java/com/conferencemanagement/conference/models/User.java
@@ -7,11 +7,13 @@ package com.conferencemanagement.conference.models;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -41,6 +43,7 @@ public class User implements Serializable {
     @Column(name = "log")
     private boolean log;
     
+    @OneToOne(cascade = CascadeType.PERSIST,mappedBy = "User")
     private Role role;
     
     List<Reservation> Reservations;
