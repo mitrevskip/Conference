c51f21b8847429036590a5a86c28c9496a4f0df5
Mario <32096232+MarioBozinovski93@users.noreply.github.com>
2017-10-12 12:28:30 +0200

    DAO-LAyer( bez room FK )

--- src/main/java/com/conferencemanagement/conference/models/Reservation.java
+++ src/main/java/com/conferencemanagement/conference/models/Reservation.java
@@ -7,6 +7,8 @@ package com.conferencemanagement.conference.models;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -23,22 +25,20 @@ import javax.persistence.Table;
  * @author Petar
  */
 @Entity
-@Table(name = "Reservation")
 public class Reservation implements Serializable{
     
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "RES_ID")
     private int resId;
     
-    private LocalDateTime meetStarts;
-    private LocalDateTime meetEnds;
+    private Date meetStarts;
+    private Date meetEnds;
     
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
-    @JoinColumn(name = "ROOM_ID")
-    private Room room;
+//    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+//    @JoinColumn(name = "ROOM_ID")
+//    private Room room;
     
     public int getResId() {
         return resId;
@@ -48,30 +48,65 @@ public class Reservation implements Serializable{
         this.resId = resId;
     }
 
-    public LocalDateTime getMeetStarts() {
+    public Date getMeetStarts() {
         return meetStarts;
     }
 
-    public void setMeetStarts(LocalDateTime meetStarts) {
+    public void setMeetStarts(Date meetStarts) {
         this.meetStarts = meetStarts;
     }
 
-    public LocalDateTime getMeetEnds() {
+    public Date getMeetEnds() {
         return meetEnds;
     }
 
-    public void setMeetEnds(LocalDateTime meetEnds) {
+    public void setMeetEnds(Date meetEnds) {
         this.meetEnds = meetEnds;
     }
 
-    public Room getRoom() {
-        return room;
+//    public Room getRoom() {
+//        return room;
+//    }
+//
+//    public void setRoom(Room room) {
+//        this.room = room;
+//    }
+//    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.resId;
+        hash = 59 * hash + Objects.hashCode(this.meetStarts);
+        hash = 59 * hash + Objects.hashCode(this.meetEnds);
+        return hash;
     }
 
-    public void setRoom(Room room) {
-        this.room = room;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Reservation other = (Reservation) obj;
+        if (this.resId != other.resId) {
+            return false;
+        }
+        if (!Objects.equals(this.meetStarts, other.meetStarts)) {
+            return false;
+        }
+        if (!Objects.equals(this.meetEnds, other.meetEnds)) {
+            return false;
+        }
+        return true;
     }
-    
+
+  
     
 
     
