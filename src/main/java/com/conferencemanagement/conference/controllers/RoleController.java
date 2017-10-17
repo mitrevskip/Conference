f03fc8c2811f47461200acafc649ae8a51582008
Petar Mitrevski <mitrevski.p@gmail.com>
2017-10-17 11:26:23 +0200

    controler user

---
+++ src/main/java/com/conferencemanagement/conference/controllers/RoleController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.conferencemanagement.conference.controllers;
+
+/**
+ *
+ * @author Petar
+ */
+public class RoleController {
+    
+}
